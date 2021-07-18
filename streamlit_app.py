394b13b0e1c035cc723942e5b136e4e1349c63f4
xieli2021 <87457886+xieli2021@users.noreply.github.com>
2021-07-18 21:10:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Name: LI XIE
+CS605: Section 1
+Data: California Fire incidents data
+URL: https://share.streamlit.io/xieli2021/web_test/main
+Description:
+This program focus on time to event (TTE) data analysis. The application will show: 
+a.	The distribution of time interval between two fire incidents will be display.
+b.	The trend of fire accidents.
+c.	The seasonal of fire accidents.
+"""
+
 import streamlit as st
-# To make things easier later, we're also importing numpy and pandas for
-# working with sample data.
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import math
+from datetime import datetime
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
 from reliability.Distributions import Weibull_Distribution
@@ -96,3 +109,267 @@ st.write("The Weibull distribution is similar to the exponential distribution. W
 #results = Fit_Everything(failures=t)
 #st.pyplot(fig)
 
+class time_series_simulator():
+    
+    def __init__(self):
+        '''
+        initialize
+        '''
+        pass
+    
+    def white_noise(self,length,mean_wn=0,variance_wn=4,radius_wn=2,
+                   distribution_wn='Guassian',plot=False):
+        '''
+        Simulate white noise
+        '''
+        if distribution_wn=='Guassian':
+            wn=np.random.normal(loc=mean_wn,
+                               scale=variance_wn,
+                               size=length)
+        elif distribution_wn=='Uniform':
+            wn=np.random.uniform(low=-radius_wn,
+                                up=radius_wn,
+                                size=length)
+        if plot:
+            plt.plot(wn)
+            plt.figure()
+        return(wn)
+    
+    def poisson(self,length,lam=1011,plot=False):
+        '''
+        Simulate Poisson distribution
+        '''
+        pois=np.random.poisson(lam=lam, size=length)
+        if plot:
+            plt.plot(pois)
+            plt.figure()
+        return(pois)
+
+    def random_walk(self,length,first_value_rw=0,drift_rw=0,
+                    mean_wn=0,variance_wn=4,radius_wn=2,
+                    distribution_wn='Guassian',
+                    plot=False,constraint_positive=False):
+        '''
+        Simulate white noise
+        If drift is not equal to 0, include trend
+        '''
+        wn=self.white_noise(length=length,mean_wn=mean_wn,variance_wn=variance_wn,
+                            radius_wn=radius_wn,distribution_wn=distribution_wn)
+        rw=np.zeros(length)
+        rw[0]=first_value_rw
+        for i in np.r_[1:length]:
+            rw[i]=first_value_rw+np.sum(wn[range(i)])+drift_rw*i
+            if constraint_positive:
+                if rw[i]<0:
+                    rw[i]=0
+        if plot:
+            plt.plot(rw)
+            plt.figure()
+        return(rw)
+
+    def linear_trend(self,length,slope=1,first_value=0):
+        '''
+        Simulate simple linear trend
+        '''
+        ts=np.r_[0:length]
+        ts=first_value+ts*slope
+        return(ts)
+
+    def auto_regression(self,length,first_value_ar=0,slope=1,constant=1,
+                        mean_wn=0,variance_wn=4,radius_wn=2,
+                        distribution_wn='Guassian',plot=False,
+                        constraint_positive=False):
+        '''
+        Simulate autoregression proccess
+        '''
+        wn=self.white_noise(length=length,mean_wn=mean_wn,variance_wn=variance_wn,
+                            radius_wn=radius_wn,distribution_wn=distribution_wn)
+        ar=np.zeros(length)
+        ar[0]=first_value_ar
+        for i in np.r_[1:length]:
+            ar[i]=ar[i-1]*slope+constant+wn[i]
+            if constraint_positive:
+                if ar[i]<0:
+                    ar[i]=0
+        if plot:
+            plt.plot(ar)
+            plt.figure()
+        return(ar)
+
+    def seasonal(self,length,first_season,season_coef=1,
+                 season_cal='additive',
+                 mean_wn=0,variance_wn=4,radius_wn=2,
+                 distribution_wn='Guassian',plot=False,
+                 constraint_positive=False):
+        '''
+        Simulate seasonal time series
+        '''
+        season_len=len(first_season)
+        season_time=math.ceil(length/season_len)
+        first_season=np.array(first_season)
+        ts=np.array(first_season)
+        for t in np.r_[1:season_time]:
+            wn=self.white_noise(length=season_len,mean_wn=mean_wn,
+                                variance_wn=variance_wn,radius_wn=radius_wn,
+                                distribution_wn=distribution_wn)
+            if season_cal=='additive':
+                season_uni=first_season+season_coef+wn
+            elif season_cal=='multiplicative':
+                season_uni=first_season*season_coef+wn
+            if constraint_positive:
+                season_uni[season_uni<0]=0
+            ts=np.append(ts,season_uni)
+        ts=ts[range(length)]
+        if plot:
+            plt.plot(ts)
+            plt.figure()
+        return(ts)
+    
+    def diff_days(self,first_date,last_date):
+        '''
+        Calculate difference of days between first and last date
+        '''
+        first_date=pd.to_datetime(first_date)
+        last_date=pd.to_datetime(last_date)
+        days=(last_date-first_date).days
+        return(days)
+    
+    def convert_df(self,first_date,ts,title='',file_name='',
+                   constraint_int=True,plot=True,write=False):
+        '''
+        Convert to dataframe
+        '''
+        df=np.zeros([len(ts),3])
+        df=pd.DataFrame(df)
+        df.columns=['ds','days','y']
+        df.ds=pd.to_datetime(first_date)
+        df.days=range(len(ts))
+        df.days=pd.to_timedelta(df.days,unit='D')
+        df.ds=df.ds+df.days
+        df.y=ts
+        df=df.loc[:,['ds','y']]
+        if constraint_int:
+            df.y=df.y.astype(int)
+        if plot:
+            df.plot.line(x='ds',y='y',
+                         legend=False,
+                         title=title)
+            plt.xlabel('')
+            plt.figure()
+        if write:
+            write_time=str(datetime.now())[:19].replace(' ','-').replace(':','-')
+            file_name=file_name+write_time+'.csv'
+            df.to_csv(file_name,index=False)
+        return(df)
+
+    def comprise(self,first_date,component_s,method='additive',file_name='',
+                 title='',plot=True,return_df=True,write=True):
+        '''
+        Comprise several components
+        '''
+        length=len(component_s[0])
+        for i in np.r_[1:len(component_s)]:
+            if len(component_s[i])!=length:
+                print('Warning!!! Error in length!!!')
+        if method=='additive':
+            ts=np.zeros(length)
+            for component in component_s:
+                ts=ts+component
+        elif method=='multiplicative':
+            ts=np.repeat(1,length)
+            for component in component_s:
+                ts=ts*component
+        df=self.convert_df(first_date=first_date,ts=ts,file_name=file_name,
+                           title=title,plot=plot,write=write)
+        if return_df:
+            return(df)
+        else:
+            return(ts)
+        
+    def discount(self,df,discount_start_date,discount_end_date,
+                 discount_increase_ratio=1.6,
+                 discount_increase_amount=2000,
+                 method='multiplicative',file_name='',
+                 title='',plot=True,return_df=True,write=True):
+        discount_start_date=pd.to_datetime(discount_start_date)
+        discount_end_date=pd.to_datetime(discount_end_date)
+        df['discount_or_not']='normal'
+        df.loc[(df.ds>discount_start_date)&
+               (df.ds<discount_end_date),'discount_or_not']='discount'
+        if method=='additive':
+            df.loc[(df.ds>discount_start_date)&
+                   (df.ds<discount_end_date),'y']=df.loc[(df.ds>discount_start_date)&
+                                                         (df.ds<discount_end_date),'y']+discount_increase_amount
+        elif method=='multiplicative':
+            df.loc[(df.ds>discount_start_date)&
+                   (df.ds<discount_end_date),'y']=df.loc[(df.ds>discount_start_date)&
+                                                         (df.ds<discount_end_date),'y']*discount_increase_ratio
+        if plot:
+            sns.scatterplot(data=df,
+                             x='ds',
+                             y='y',
+                             hue='discount_or_not')
+            plt.xlabel('')
+            plt.figure()
+        if write:
+            write_time=str(datetime.now())[:19].replace(' ','-').replace(':','-')
+            file_name=file_name+write_time+'.csv'
+            df.to_csv(file_name,index=False)
+        if return_df:
+            return(df)
+        else:
+            return(df.y.values)
+    
+    def holiday(self,df,holiday_md=['-01-01','-12-25','-05-01'],
+                 holiday_increase_ratio=1.6,
+                 holiday_increase_amount=2000,
+                 method='multiplicative',file_name='',
+                 title='',plot=True,return_df=True,write=True):
+        df['holiday_or_not']='normal'
+        # get unique year list
+        year_unique=set(df.ds.dt.year)
+        y=[]
+        for i in year_unique:
+            y.append(str(i))
+        y_c=np.repeat(y,len(holiday_md))
+        holiday_c=np.tile(holiday_md,len(y))
+        holiday_all=[]
+        # get all holiday in all year
+        for i in range(len(holiday_c)):
+            h=str(y_c[i])+holiday_c[i]
+            h=pd.to_datetime(h)
+            holiday_all.append(h)
+        df.loc[np.isin(df.ds,holiday_all),'holiday_or_not']='holiday'
+        
+        if method=='additive':
+            df.loc[np.isin(df.ds,holiday_all),'y']=df.loc[np.isin(df.ds,holiday_all),'y']+\
+                holiday_increase_amount
+        elif method=='multiplicative':
+            df.loc[np.isin(df.ds,holiday_all),'y']=df.loc[np.isin(df.ds,holiday_all),'y']*holiday_increase_ratio
+        if plot:
+            sns.scatterplot(data=df,
+                             x='ds',
+                             y='y',
+                             hue='holiday_or_not')
+            plt.xlabel('')
+            plt.figure()
+        if write:
+            write_time=str(datetime.now())[:19].replace(' ','-').replace(':','-')
+            file_name=file_name+write_time+'.csv'
+            df.to_csv(file_name,index=False)
+        if return_df:
+            return(df)
+        else:
+            return(df.y.values)
+        
+first_date='2012-01-01'
+last_date='2021-01-01'
+
+tss=time_series_simulator()
+ts_len=tss.diff_days(first_date,last_date)
+ts_len
+
+fig=plt.figure()
+ts_basic=tss.random_walk(length=ts_len,first_value_rw=10,drift_rw=0,
+                         mean_wn=0,variance_wn=11,plot=True)
+st.pyplot(fig)
