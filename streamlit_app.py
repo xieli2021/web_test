438bce4c1e62112164d6715e17490a0a13d40b39
xieli2021 <87457886+xieli2021@users.noreply.github.com>
2021-07-18 18:53:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,22 +5,4 @@ import seaborn as sns
 
 st.title('My first app')
 
-url='https://raw.githubusercontent.com/xieli2021/web_test/main/California_Fire_Incidents%20(1).csv'
-
-def read_clean(url):
-    dfR=pd.read_csv(url)
-    df=dfR.copy()
-    df=df.loc[:,['Counties','Started','Extinguished']]
-    df.columns=['country','start','end']
-    df.start=pd.to_datetime(df.start)
-    df.end=pd.to_datetime(df.end)
-    df=df.sort_values('start')
-    df=df.loc[df.start>'2013-01-01',:]
-    df['t']=0
-    df=df.reset_index(drop=True)
-    for i in np.r_[1:df.shape[0]]:
-        df.loc[i,'t']=(df.start[i]-df.start[i-1])/np.timedelta64(1, 'D')
-    return(df)
-
-df=read_clean(url)
 
