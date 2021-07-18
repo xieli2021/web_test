d3139db38276050ab5d174a51e7afefe7bdaf4e2
xieli2021 <87457886+xieli2021@users.noreply.github.com>
2021-07-18 20:15:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
 import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
+from reliability.Distributions import Weibull_Distribution
+from reliability.Probability_plotting import plot_points
+from reliability.Fitters import Fit_Weibull_3P
+from reliability.Fitters import Fit_Weibull_2P
 
 url='https://raw.githubusercontent.com/xieli2021/web_test/main/California_Fire_Incidents%20(1).csv'
 
@@ -41,6 +48,25 @@ st.write("The table below shows start time, end time, location (contry) and time
 
 st.write(df)
 
+st.write("The Figure below show California WildFires incidents by month.")
+
+fig=plt.figure()
+ax=sns.lineplot(data=cdf,
+             x='ym',
+             y='number')
+plt.xlabel('')
+plt.ylabel('Fires incidents by month')
+st.pyplot(fig)
+
+st.write("Decompose time series to trend, seasonality noise.")
+result=seasonal_decompose(cdf['number'], 
+                          model='additive', 
+                          period=12)
+fig=result.plot()
+st.pyplot(fig)
+st.write("Both seasonal and trends exist. There are more and moree fire incidents with time advances. And more incidents happen in summer.")
+
+
 st.write("The Figure below show distribution of time to next California WildFires incidents.")
 fig=plt.figure()
 ax=sns.kdeplot(df.t)
@@ -54,12 +80,16 @@ ax=sns.scatterplot(data=df,
                    y='t')
 st.pyplot(fig)
 
-st.write("The Figure below show California WildFires incidents by month.")
-st.write("It seems seasonal exist but no trends.")
+st.write("Fit time to next California WildFires incidents to Weibull distribution .")
+t=df.t.values
 fig=plt.figure()
-ax=sns.lineplot(data=cdf,
-             x='ym',
-             y='number')
-plt.xlabel('')
-plt.ylabel('Fires incidents by month')
+weibull_fit = Fit_Weibull_2P(failures=t,show_probability_plot=False,print_results=False)
+weibull_fit.distribution.SF(label='Fitted Distribution',color='steelblue')
+plot_points(failures=t,func='SF',label='failure data',color='red',alpha=0.7)
+plt.legend()
 st.pyplot(fig)
+st.write("It looks fit well.")
+st.write("The Weibull distribution is similar to the exponential distribution. While the exponential distribution assumes a constant hazard, the Weibull distribution assumes a monotonic hazard that can either be increasing or decreasing but not both. It has two parameters. The shape parameter (σ ) controls whether hazard increases (σ<1 ) or decreases (σ>1 ) (in the exponential distribution, this parameter is set to 1). The scale parameter, (1/σ)exp(-β0/σ), determines the scale of this increase/decrease. Since the Weibull distribution simplifies to the exponential distribution when σ=1, the null hypothesis that σ=1 can be tested using a Wald test. The main advantage of this model is that it is both a PH and AFT model, so both hazard ratios and time ratios can be estimated. Again, the main drawback is that the assumption of monotonicity of the baseline hazard may be implausible in some cases.")
+
+
+
