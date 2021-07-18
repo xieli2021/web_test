3d919482a4ff1571dd05039d5df7dee98c131120
xieli2021 <87457886+xieli2021@users.noreply.github.com>
2021-07-18 18:59:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,22 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.title('My first app')
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+url='https://raw.githubusercontent.com/xieli2021/web_test/main/California_Fire_Incidents%20(1).csv'
 
+def read_clean(url):
+    dfR=pd.read_csv(url)
+    df=dfR.copy()
+    df=df.loc[:,['Counties','Started','Extinguished']]
+    df.columns=['country','start','end']
+    df.start=pd.to_datetime(df.start)
+    df.end=pd.to_datetime(df.end)
+    df=df.sort_values('start')
+    df=df.loc[df.start>'2013-01-01',:]
+    df['t']=0
+    df=df.reset_index(drop=True)
+    for i in np.r_[1:df.shape[0]]:
+        df.loc[i,'t']=(df.start[i]-df.start[i-1])/np.timedelta64(1, 'D')
+    return(df)
 
+df=read_clean(url)
+st.write(df)
